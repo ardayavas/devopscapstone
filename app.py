3c5531967142b220123d6e260e3ececaa2d09ca4
Arda Yavas <ardayavas@gmail.com>
2019-11-14 21:03:46 +0300

    Rolling 2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World, my name is Arda Yavas - Rolling 1'
+    return 'Hello World, my name is Arda Yavas - Rolling 2'
 
 if __name__ == "__main__": # pragma: no cover
     log.info("START Flask")
